90549da9d57c327315326d4881260661a2b26315
Sapfirka <sapfirka@mail.ru>
2023-01-25 15:49:26 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 15400;
+        int bonus = price/20;
+
+        System.out.println("Начислено бонусов");
+        System.out.println(bonus);
+    }
+}
